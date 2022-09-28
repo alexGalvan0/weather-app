28104d5fee6f3b6431b48e7d9ff9da685344752d
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-09-28 20:36:09 +0000

    js

--- script.js
+++ script.js
@@ -9,34 +9,34 @@ function creatElment(elment='div'){
 }
 function createFormDiv(){
 
-//formContainer
-let formDivContainer = creatElment('div')
-formDivContainer.classList.add('row','row-zip')
-root.appendChild(formDivContainer)
+    //formContainer
+    let formDivContainer = creatElment('div')
+    formDivContainer.classList.add('row','row-zip')
+    root.appendChild(formDivContainer)
 
-//form column
-let formCol = creatElment()
-formCol.classList.add('col', 'd-flex', 'justify-content-center')
-formDivContainer.appendChild(formCol)
+    //form column
+    let formCol = creatElment()
+    formCol.classList.add('col', 'd-flex', 'justify-content-center')
+    formDivContainer.appendChild(formCol)
 
-//form label
-let formLabel = creatElment('label');
-formLabel.innerHTML='ZIP: '
+    //form label
+    let formLabel = creatElment('label');
+    formLabel.innerHTML='ZIP: '
 
-//form input
-let inputField = creatElment('input')
-inputField.type = 'text'
-inputField.value = '40517'
-inputField.id = 'zipInput'
-formLabel.append(inputField)
-formDivContainer.appendChild(formLabel)
+    //form input
+    let inputField = creatElment('input')
+    inputField.type = 'text'
+    inputField.value = '40517'
+    inputField.id = 'zipInput'
+    formLabel.append(inputField)
+    formDivContainer.appendChild(formLabel)
 
-//button
-let btn = creatElment('button')
-btn.innerHTML = 'Get Weather'
-btn.classList.add('btn', 'btn-primary')
-btn.id = 'getWeatherBtn';
-formCol.appendChild(btn)
+    //button
+    let btn = creatElment('button')
+    btn.innerHTML = 'Get Weather'
+    btn.classList.add('btn', 'btn-primary')
+    btn.id = 'getWeatherBtn';
+    formCol.appendChild(btn)
 }
 function createCityDiv(){
     //city row
@@ -148,3 +148,55 @@ createCityDiv()
 createTempDiv()
 createConditionDiv()
 createInfo()
+
+const zipInput = document.getElementById('zipInput');
+const getWeatherBtn =document.getElementById('getWeatherBtn');
+const cityOutput = document.getElementById('cityOutput');
+const kelvin = document.getElementById('kelvin');
+const fere = document.getElementById('fere')
+const celsius = document.getElementById('celsius');
+let condition = document.getElementById('condition');
+let icon = document.getElementById('icon') 
+
+function createHtmlElement(element='div',nameClass){
+    return document.createElement(element).classList.add(nameClass)
+}
+
+createHtmlElement('input')
+
+ async function getUserInput(){
+    let zip = zipInput.value.toString();
+    const key = '933f7703450958683b430c05ee91f80b';
+    let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${key}`
+ 
+    let resp = await axios.get(url);
+    let data = resp;
+    console.log(data)
+    
+
+
+
+    let cityName = data.data.name;
+    let fereTemp = data.data.main.temp;
+
+    let celsiTemp = (parseInt(fereTemp) - 32 ) * 5/9
+    let kelvinTemp = (parseInt(fereTemp) - 32) * 5/9 + 273.15
+
+    let conditionTemp = data.data.weather[0].description
+    
+    let iconId = data.data.weather[0].icon
+
+
+    cityOutput.innerHTML = cityName
+
+    fere.innerText = `Fahrenheit: ${Math.ceil(fereTemp)}`
+    celsius.textContent = `Celsius: ${Math.ceil(celsiTemp)}`
+    kelvin.textContent = `Kelvin: ${Math.ceil(kelvinTemp)}`
+    
+    condition.textContent = conditionTemp;
+
+    icon.src = `http://openweathermap.org/img/wn/${iconId}@2x.png`
+
+}
+
+getWeatherBtn.addEventListener('click',getUserInput)
